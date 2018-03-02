9c70f69ddcaaabc0aa583210217134c2319f3d60
Jon Hodgins <jon_hodgins@outlook.com>
2018-03-01 17:40:02 -0800

    Completed isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,13 @@
+import re
+
+
 def is_isogram(string):
-    pass
+    if string == "":
+        return True
+    else:
+        string = re.sub("[^a-zA-z]+", "", string)  # Remove non-alpha chars
+        string = string.lower()
+        for letter in string:
+            if string.count(letter) > 1:
+                return False
+        return True
